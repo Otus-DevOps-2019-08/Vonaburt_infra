27a3f68f6593eaaa20265d1570fd8bd82406b1b9
Vadim Trubanov <vonaburt@gmail.com>
2019-09-22 19:07:49 +0300

    Updated install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B6B
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
+
